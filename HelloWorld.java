a86bdc656f15457d26b33837114972a815bf0051
Pratham Pawar <vu4f2324148@pvppcoe.ac.in>
2025-09-19 12:53:13 +0530

    Initial Commit  exp 8 DevOps Lab

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, DevOps World!");
+        System.out.println("This Java program was executed successfully by Jenkins.");
+    }
+}
